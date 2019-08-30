57c81fdff938461db50803625133ac36283c4e1f
Mgeeeeee <610814162@qq.com>
2019-08-30 10:23:14 +0800

    Update and rename 1.py to Code/1.py

---
+++ Code/1.py
@@ -0,0 +1,9 @@
+fake = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+key = 'map'
+
+intab = 'abcdefghijklmnopqrstuvwxyz'
+outtab = 'cdefghijklmnopqrstuvwxyzab'
+trantab = fake.maketrans(intab,outtab)
+
+print(fake.translate(trantab))
+print(key.translate(trantab))
